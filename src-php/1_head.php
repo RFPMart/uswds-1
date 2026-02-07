a0b8418fa9bd2c9e79838c874b292f7e601c84eb
Selva-DevEyes <selvamshunmugam28@gmai.com>
2026-02-07 17:34:27 +0530

    Overview the Code

---
+++ src-php/1_head.php
@@ -0,0 +1,15 @@
+<!-- ========================= USWDS Init Script ========================= -->
+<script src="uswds/dist/js/uswds-init.min.js"></script>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+<!-- ========================= Stylesheets ========================= -->
+<link rel="stylesheet" href="uswds/dist/css/uswds.min.css" />
+<link rel="stylesheet" href="uswds/dist/css/uswds.css" />
+<link rel="stylesheet" href="uswds/dist/css/stylesheets/uswds.css" />
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+
+
+<!-- ========================= Font Awesome Icons ========================= -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
